2f3c4b4934d1eff43a45ca4aa0bdae95779896e9
GENRAY1 <genray@ro.ru>
2024-07-31 23:35:07 +0300

    test

--- backend/PetPalsProfile.Identity/Identity.Application/DependencyInjection.cs
+++ backend/PetPalsProfile.Identity/Identity.Application/DependencyInjection.cs
@@ -10,7 +10,7 @@ public static class DependencyInjection
         {
             configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
         });
-
+    
         return services;
     }
 }
